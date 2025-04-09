9e9f333f2bbe80d34bdff2bf317779c9f780a023
6kaua9 <0kauagp@gmail.com>
2025-04-09 13:06:55 -0300

    primeira parte
    Interface simples e botão criar salvando o nome do quadro em local storage

---
+++ trelolo.js
@@ -0,0 +1,31 @@
+function criarQuadro(){
+    const quadros = JSON.parse(localStorage.getItem("quadros")) || [];
+    const nome = document.getElementById('nomeQuadro').value;
+    if(nome === ""){
+        alert("A area não pode estar vazia");
+        return;
+    }
+    for(let i = 0; i < quadros.length; i++){
+        if(quadros[i].toString() === nome){
+            alert("Já existe um quadro com este nome");
+            return;
+        }
+    }
+        quadros.push(nome);
+    localStorage.setItem("quadros", JSON.stringify(quadros));
+    alert("Novo quadro criado");
+}
+
+function botaoNovoQuadro(){
+    console.log("clickado");
+    const doc = document.getElementById("novoQuadro");
+
+        if(doc.style.display === 'none'){
+            doc.style.display = 'flex';
+            console.log(" Visivel");
+            
+        } else if(doc.style.display === "flex"){
+            doc.style.display = "none";
+            console.log("'invisivel'");
+        }
+}
